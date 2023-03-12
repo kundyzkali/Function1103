74c08f1ded62f84cabfdb354ce14a7041b6e0e79
Kundyz <kundyzkali2022@gmail.com>
2023-03-12 15:59:10 +0600

    Tasks1203

---
+++ task042/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+int num = Prompt("Введите число: ");
+int found = Prompt("Введите целое десятичное число основание системы исчесления: ");
+Console.WriteLine(DecToBin(num, found));
+Console.WriteLine(DecToBin(num));
+ 
+ int DecToBin(int num)
+{
+    int res = 0;
+    int multiplier = 1;
+    while(num > 0)
+    {
+     res += (num % 2)* multiplier; 
+     num = num / 2;
+     multiplier *=10;
+    }
+return res;
+}
+
+int Prompt(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
